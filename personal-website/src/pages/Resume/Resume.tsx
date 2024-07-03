832c9c73654e84aa20c8467afc72b8875ba371ad
Carter Moseley <cartermoseley20@gmail.com>
2024-07-02 22:26:44 -0500

    Updated the resume back to iFrame and added pdf fix to readme

--- personal-website/src/pages/Resume/Resume.tsx
+++ personal-website/src/pages/Resume/Resume.tsx
@@ -12,8 +12,7 @@ export default function MyApp() {
 
     return (
         <div className='resume-div'>
-            {/* <iframe src={'/resume.pdf'} className='resume-image' /> */}
-            <object data="./resume.pdf" type="application/pdf" className='resume-image' width="100%" height="100%" />
+            <iframe src={'/resume.pdf'} className='resume-image' />
         </div>
     );
 }
